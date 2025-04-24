f23eb22b77418eb6352340e18f8e0ad6e5607613
wei756 <wei756fg@gmail.com>
2025-04-25 04:00:02 +0900

    refactor: 상품 관리 페이지 불필요 클래스 제거, Button 컴포넌트로 교체

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,39 +1,30 @@
-import { WhitePlusIcon } from '@/assets/icon/whiteplusicon';
+import { Button } from '@/components/common';
 import { CategorySelect, ProductItem } from '@/components/product';
 
 export const ProductPage = () => {
   return (
-    <div className="flex h-screen">
-      <div className="flex flex-col w-full h-auto">
-        <div className="w-full h-full flex flex-col gap-6 p-8 h-screen">
-          <div className="w-full h-auto gap-3 flex flex-col">
-            <div className="w-full h-auto flex items-center justify-between">
-              <div className="leading-6 font-semibold text-2xl text-center">
-                상품
-              </div>
-              <div className="w-auto h-auto gap-2.5 flex items-center p-3 bg-[#0092CA] rounded-xl">
-                <WhitePlusIcon />
-                <div className="text-white leading-6">상품 등록</div>
-              </div>
-            </div>
-            <div className="w-full h-auto flex justify-between items-center">
-              <CategorySelect />
-              <div className="flex gap-9 px-1">
-                <div>사장님추천</div>
-                <div>품절표시</div>
-              </div>
-            </div>
+    <div className="flex flex-col gap-6 p-8 w-full h-full">
+      <div className="w-full gap-3 flex flex-col">
+        <header className="w-full flex items-center justify-between">
+          <div className="font-semibold text-2xl">상품</div>
+          <Button>상품 등록</Button>
+        </header>
+        <div className="w-full flex justify-between items-center">
+          <CategorySelect />
+          <div className="flex gap-9 px-1">
+            <div>사장님추천</div>
+            <div>품절표시</div>
           </div>
-          <ProductItem
-            image=""
-            name="상품명"
-            price={0}
-            description="상품 설명"
-            recommended={false}
-            soldOut={false}
-          />
         </div>
       </div>
+      <ProductItem
+        image=""
+        name="상품명"
+        price={0}
+        description="상품 설명"
+        recommended={false}
+        soldOut={false}
+      />
     </div>
   );
 };
