67afcfa47432b02ac03f809d0657964fdbe8b615
wei756 <wei756fg@gmail.com>
2025-04-26 00:22:38 +0900

    fix: Aside 헤더 문구 수정

--- src/components/common/Aside/Aside.tsx
+++ src/components/common/Aside/Aside.tsx
@@ -3,9 +3,10 @@ import { LogoutRounded } from '@mui/icons-material';
 import { AsideItem } from './components';
 import { list } from './constants';
 import { useCommonValues } from '@/contexts/common/CommonContext';
+import { Button } from '../Button/Button';
 
 export const Aside = () => {
-  const { storeName, isAsideOpened } = useCommonValues();
+  const { isAsideOpened } = useCommonValues();
 
   return (
     <motion.aside
@@ -16,10 +17,10 @@ export const Aside = () => {
     >
       <div className="flex flex-col gap-6 py-8 min-w-64 w-64 h-full bg-gray-200">
         <header className="flex flex-row items-center justify-between px-8">
-          <span className="text-xl leading-none font-semibold">
-            {storeName}
-          </span>
-          <LogoutRounded fontSize="medium" />
+          <span className="text-xl leading-none font-semibold">야밤</span>
+          <Button isNoPadding color="tertiary-transparent">
+            <LogoutRounded fontSize="medium" />
+          </Button>
         </header>
         <ul className="flex flex-col px-4 items-center w-full h-0 flex-1 gap-2">
           {list.map((item, index) => (
