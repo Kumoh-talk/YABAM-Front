fec74f8b7d998e9ea5fd3d5ea66297147c891715
Y-seungyi <kimche9865@naver.com>
2025-04-25 20:34:00 +0900

    fix: css수정

--- src/components/product/PlusProduct/PlusProduct.tsx
+++ src/components/product/PlusProduct/PlusProduct.tsx
@@ -12,17 +12,17 @@ export const PlusProduct = () => {
                 </div>
                 <div className="flex flex-col gap-4">
                     <div className="px-4">이름</div>
-                    <input className="w-full px-4" type="text" placeholder="메뉴 이름" />
+                    <input className="w-full" type="text" placeholder="메뉴 이름" />
                 </div>
                 <div className="flex flex-col gap-4">
-                    <div className=" px-4">가격</div>
+                    <div className="px-4">가격</div>
                     <div className="relative">
-                        <input className="w-full px-4 remove-arrow" type="number" />
+                        <input className="w-full remove-arrow" type="number" />
                         <span className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-700">원</span>
                     </div>
                 </div>
                 <div className="flex flex-col gap-4">
-                    <div className=" px-4">설명</div>
+                    <div className="px-4">설명</div>
                     <textarea
                         className="w-full px-4 py-2 border border-gray-500 focus:outline-none focus:ring-1 focus:ring-primary focus:border-transparent rounded-md"
                         placeholder="설명입니다"
