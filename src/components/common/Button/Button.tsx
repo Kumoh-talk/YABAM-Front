b2ad03c0d774fbe9342845fd2f3b0cbcc0383cdc
wei756 <wei756fg@gmail.com>
2025-05-01 02:29:11 +0900

    feat: Button 컴포 href 속성 추가

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { CommonLink } from '../CommonLink/CommonLink';
 
 export type ButtonColor =
   | 'primary'
@@ -15,6 +16,7 @@ export type ButtonColor =
   | 'white-transparent';
 export interface Props {
   className?: string;
+  href?: string;
   children?: React.ReactNode;
   color?: ButtonColor;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
@@ -23,29 +25,37 @@ export interface Props {
 
 export const Button = (props: Props) => {
   const color = props.color || 'primary';
+
+  const className = clsx(
+    'flex flex-row justify-center items-center rounded-lg text-base leading-none font-medium cursor-pointer',
+    props.className,
+    {
+      'px-3 h-10': !props.isNoPadding,
+      'bg-primary text-text-dark-primary': color === 'primary',
+      'bg-secondary text-text-dark-primary': color === 'secondary',
+      'bg-tertiary text-text-dark-primary': color === 'tertiary',
+      'bg-red text-text-dark-primary': color === 'red',
+      'bg-black text-text-dark-primary': color === 'black',
+      'bg-white text-text-primary': color === 'white',
+      'bg-transparent text-primary': color === 'primary-transparent',
+      'bg-transparent text-secondary': color === 'secondary-transparent',
+      'bg-transparent text-tertiary': color === 'tertiary-transparent',
+      'bg-transparent text-red': color === 'red-transparent',
+      'bg-transparent text-black': color === 'black-transparent',
+      'bg-transparent text-white': color === 'white-transparent',
+    },
+  );
+
+  if (props.href) {
+    return (
+      <CommonLink className={className} href={props.href}>
+        {props.children}
+      </CommonLink>
+    );
+  }
+
   return (
-    <button
-      className={clsx(
-        'flex flex-row justify-center items-center rounded-lg text-base leading-none font-medium cursor-pointer',
-        props.className,
-        {
-          'px-3 h-10': !props.isNoPadding,
-          'bg-primary text-text-dark-primary': color === 'primary',
-          'bg-secondary text-text-dark-primary': color === 'secondary',
-          'bg-tertiary text-text-dark-primary': color === 'tertiary',
-          'bg-red text-text-dark-primary': color === 'red',
-          'bg-black text-text-dark-primary': color === 'black',
-          'bg-white text-text-primary': color === 'white',
-          'bg-transparent text-primary': color === 'primary-transparent',
-          'bg-transparent text-secondary': color === 'secondary-transparent',
-          'bg-transparent text-tertiary': color === 'tertiary-transparent',
-          'bg-transparent text-red': color === 'red-transparent',
-          'bg-transparent text-black': color === 'black-transparent',
-          'bg-transparent text-white': color === 'white-transparent',
-        },
-      )}
-      onClick={props.onClick}
-    >
+    <button className={className} onClick={props.onClick}>
       {props.children}
     </button>
   );
