86cf96a5c3b1f8f5fb9f370455d706229cdf3d8e
wei756 <wei756fg@gmail.com>
2025-05-14 04:40:48 +0900

    feat: api 요청시 엑세스 토큰 삽입 구현, ngrok 대응, PATCH 메소드 enum 추가

--- src/utils/api/backend/common.ts
+++ src/utils/api/backend/common.ts
@@ -1,16 +1,29 @@
-import { ApiError, ApiResponse } from "@/types/backend";
+import { ApiError, ApiResponse } from '@/types/backend';
+import { Cookies } from 'react-cookie';
+
+const cookies = new Cookies();
+
+export const getHeaders = () => {
+  const headers: HeadersInit = {
+    'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': 'true',
+  };
+  const accessToken = cookies.get('access_token');
+  if (accessToken) {
+    headers['Authorization'] = `Bearer ${accessToken}`;
+  }
+  return headers;
+};
 
 export const api = async <T>(
   path: string,
-  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  method: 'GET' | 'POST' | 'PATCH' | 'DELETE',
   body?: Record<string, string | number | boolean>,
 ): Promise<ApiResponse<T> | ApiError> => {
   try {
     const res = await fetch(`${import.meta.env.VITE_API_BASE_URI}${path}`, {
       method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: getHeaders(),
       body: JSON.stringify(body),
     });
     return (await res.json()) as ApiResponse<T> | ApiError;
