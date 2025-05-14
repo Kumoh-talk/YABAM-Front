7b26bc32d68c2c27640cc671842451cee8f3f454
Y-seungyi <kimche9865@naver.com>
2025-05-15 02:44:51 +0900

    feat: 카테고리 context 추가

---
+++ src/contexts/category/CategoryContext.tsx
@@ -0,0 +1,72 @@
+import { useCategory } from '@/hooks';
+import { Category } from '@/hooks/useCategory';
+import { useStoreValues } from '@/contexts/store/StoreContext';
+import { createContext, useContext } from 'react';
+
+export type Values = {
+  categories: Category[];
+  category: Category;
+};
+
+export type Actions = {
+  createCategory: (name: string, order: number) => Promise<void>;
+  updateCategoryName: (categoryId: number, name: string) => Promise<void>;
+  updateCategoryOrder: (categoryId: number, order: number) => Promise<void>;
+  removeCategory: (categoryId: number) => Promise<void>;
+  updateCategory: (value: Partial<Omit<Category, 'id'>>) => void;
+  refreshCategories: () => Promise<void>;
+};
+
+const CategoryValuesContext = createContext<Values | undefined>(undefined);
+const CategoryActionsContext = createContext<Actions | undefined>(undefined);
+
+export interface Props {
+  readonly children: React.ReactNode;
+}
+
+export const CategoryProvider = (props: Props) => {
+  const { store } = useStoreValues();
+  const {
+    categories,
+    category,
+    create,
+    updateName,
+    updateOrder,
+    remove,
+    refresh,
+    update,
+  } = useCategory(store.id);
+
+  return (
+    <CategoryValuesContext.Provider value={{ categories, category }}>
+      <CategoryActionsContext.Provider
+        value={{
+          createCategory: create,
+          updateCategoryName: updateName,
+          updateCategoryOrder: updateOrder,
+          removeCategory: remove,
+          updateCategory: update,
+          refreshCategories: refresh,
+        }}
+      >
+        {props.children}
+      </CategoryActionsContext.Provider>
+    </CategoryValuesContext.Provider>
+  );
+};
+
+export const useCategoryValues = () => {
+  const context = useContext(CategoryValuesContext);
+  if (context === undefined) {
+    throw new Error('useCategoryValues must be used within a CategoryProvider');
+  }
+  return context;
+};
+
+export const useCategoryActions = () => {
+  const context = useContext(CategoryActionsContext);
+  if (context === undefined) {
+    throw new Error('useCategoryActions must be used within a CategoryProvider');
+  }
+  return context;
+};
