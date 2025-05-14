75394cea4f2c60b4a436e35f09cd6f38b93d7971
wei756 <wei756fg@gmail.com>
2025-05-15 00:32:50 +0900

    fix: 시간 포맷 함수 분,초 2자리 고정

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -34,8 +34,12 @@ export const getRelativeSeconds = (date: number | string | Date) => {
 
 export const formatTimeString = (time: number) => {
   const hours = Math.floor(time / 3600_000);
-  const minutes = Math.floor((time % 3600_000) / 60_000);
-  const seconds = Math.floor((time % 60_000) / 1000);
+  const minutes = Math.floor((time % 3600_000) / 60_000)
+    .toString()
+    .padStart(2, '0');
+  const seconds = Math.floor((time % 60_000) / 1000)
+    .toString()
+    .padStart(2, '0');
 
   return `${hours}:${minutes}:${seconds}`;
 };
