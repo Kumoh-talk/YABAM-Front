43080038900e3e5da976ce23c735295700609ee7
wei756 <wei756fg@gmail.com>
2025-05-17 02:40:21 +0900

    feat: 주문 내역 페이지 일부 구현

---
+++ src/components/history/OrderHistoryList/OrderHistoryList.tsx
@@ -0,0 +1,28 @@
+import { useStoreValues } from '@/contexts/store/StoreContext';
+import { OrderHistoryItem } from './components';
+
+export interface Props {
+  saleId: number;
+}
+export const OrderHistoryList = (_: Props) => {
+  const { orders } = useStoreValues();
+
+  const list = orders.map((order) => (
+    <OrderHistoryItem key={order.orderId} order={order} />
+  ));
+  return (
+    <section className="flex flex-col gap-4 items-stretch px-8">
+      <header className="flex flex-row gap-4 px-2 font-medium text-base leading-[140%] text-center">
+        <span className="w-[11.25rem]">주문일시</span>
+        <span className="w-24">테이블 번호</span>
+        <span className="w-[4.5rem]">점유 시간</span>
+        <span className="w-0 flex-1">주문 내용</span>
+        <span className="w-20">주문 금액</span>
+        <span className="w-[80px]">테이블 사용</span>
+        <span className="w-[80px]">총 금액</span>
+        <span className="w-[87px]"></span>
+      </header>
+      <ul className="flex flex-col items-stretch">{list}</ul>
+    </section>
+  );
+};
---
+++ src/components/history/OrderHistoryList/components/OrderHistoryItem/OrderHistoryItem.tsx
@@ -0,0 +1,61 @@
+import { OrderInfo } from '@/types/backend/order';
+import {
+  formarDateString,
+  formatNumberWithComma,
+  formatTimeString,
+} from '@/utils/functions';
+
+export interface Props {
+  order: OrderInfo;
+}
+
+export const OrderHistoryItem = (props: Props) => {
+  const startedTime = props.order.receipt.receiptInfo.startUsageTime!;
+  const stoppedTime = props.order.receipt.receiptInfo.stopUsageTime;
+  const duration = stoppedTime
+    ? new Date(stoppedTime!).getTime() - new Date(startedTime).getTime()
+    : -1;
+
+  const menuStr = props.order.orderMenus
+    .map((menu) => {
+      return `${menu.menuInfo.menuName}x${menu.quantity}`;
+    })
+    .join('\n');
+
+  const startUsageTime = props.order.receipt.receiptInfo.startUsageTime!;
+  const startedTimeStr = new Date(
+    new Date(startUsageTime).getTime() -
+      new Date().getTimezoneOffset() * 60 * 1000,
+  ).toLocaleString('ko-KR', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+  });
+
+  return (
+    <li className="flex flex-row gap-4 px-2 font-medium text-base leading-[140%]">
+      <span className="w-[11.25rem] text-center">{startedTimeStr}</span>
+      <span className="w-24 text-center">
+        {props.order.receipt.tableInfo.tableNumber}번 테이블
+      </span>
+      {duration === -1 ? (
+        <span className="w-[4.5rem] text-center">점유중</span>
+      ) : (
+        <span className="w-[4.5rem] text-right">
+          {formatTimeString(duration)}
+        </span>
+      )}
+      <span className="w-0 flex-1 whitespace-pre-line">{menuStr}</span>
+      <span className="w-20 text-right">0</span>
+      <span className="w-[80px] text-right">0</span>
+      <span className="w-[80px] text-right">
+        {formatNumberWithComma(props.order.totalPrice)}
+      </span>
+      <span className="w-[87px]"></span>
+    </li>
+  );
+};
