8b3f86d6477a79d389b27122769f0bd3e450dcba
wei756 <wei756fg@gmail.com>
2025-05-16 18:21:59 +0900

    chore: react-toastify 패키지 임포트

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
+import { ToastContainer } from 'react-toastify';
 import { CommonProvider } from './contexts/common/CommonContext.tsx';
 import { StoreProvider } from './contexts/store/StoreContext.tsx';
 import { TableProvider } from './contexts/table/TableContext.tsx';
@@ -9,6 +10,7 @@ import { CategoryProvider } from './contexts/category/CategoryContext.tsx';
 import { MenuProvider } from './contexts/menu/MenuContext.tsx';
 import App from './App.tsx';
 import './styles.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -20,6 +22,13 @@ createRoot(document.getElementById('root')!).render(
               <CategoryProvider>
                 <MenuProvider>
                   <App />
+                  <ToastContainer
+                    position="bottom-right"
+                    limit={5}
+                    closeButton={false}
+                    autoClose={4000}
+                    hideProgressBar
+                  />
                 </MenuProvider>
               </CategoryProvider>
             </TableProvider>
