1da780f17666f9f838a355de9f406c5d3e0039ec
wei756 <wei756fg@gmail.com>
2025-05-16 19:49:30 +0900

    feat: Toggle 작은 사이즈 구현

--- src/components/common/Toggle/Toggle.tsx
+++ src/components/common/Toggle/Toggle.tsx
@@ -5,23 +5,30 @@ export interface Props {
   isSelected: boolean;
   onClick: () => void;
   color: 'primary' | 'secondary';
+  size?: 'normal' | 'small';
 }
 
 export const Toggle = (props: Props) => {
   const color = props.color === 'primary' ? 'bg-primary' : 'bg-secondary';
+  const size = props.size ?? 'normal';
   return (
     <button
       onClick={props.onClick}
       className={clsx(
-        'flex justify-start transition-colors duration-300 cursor-pointer w-14 p-1 rounded-full',
+        'flex justify-start transition-colors duration-300 cursor-pointer rounded-full',
         {
           'bg-gray-500': !props.isSelected,
           [color]: props.isSelected,
+          'w-14 p-1': size === 'normal',
+          'w-9 p-[.125rem]': size === 'small',
         },
       )}
     >
       <motion.div
-        className="size-6 bg-white rounded-full"
+        className={clsx('bg-white rounded-full', {
+          'size-6': size === 'normal',
+          'size-4': size === 'small',
+        })}
         animate={{
           x: props.isSelected ? '100%' : 0,
         }}
