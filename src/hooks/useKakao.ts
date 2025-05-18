1044d867040fe6e6e760b522c0810e130d6bdbe0
wei756 <wei756fg@gmail.com>
2025-05-19 06:50:08 +0900

    fix: 로그아웃 api 요청 실패해도 쿠키 삭제되게 수정

--- src/hooks/useKakao.ts
+++ src/hooks/useKakao.ts
@@ -82,6 +82,9 @@ export const useKakao = () => {
     } catch (error) {
       console.error('로그아웃 실패:', error);
       toast.error('로그아웃에 실패했습니다.');
+      removeCookie('id_token', { path: '/' });
+      removeCookie('access_token', { path: '/' });
+      removeCookie('refresh_token', { path: '/' });
       return false;
     }
   };
