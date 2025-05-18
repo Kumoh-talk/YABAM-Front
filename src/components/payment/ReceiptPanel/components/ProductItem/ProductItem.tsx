ac6b4de9cfee85c17fdbfa75b96ed97b031d5d70
wei756 <wei756fg@gmail.com>
2025-05-19 07:28:42 +0900

    feat: 영수증 패널 수량 증감 버튼 이벤트 구현

--- src/components/payment/ReceiptPanel/components/ProductItem/ProductItem.tsx
+++ src/components/payment/ReceiptPanel/components/ProductItem/ProductItem.tsx
@@ -5,22 +5,35 @@ import { OrderMenuInfo } from '@/types/backend/order';
 
 export interface Props {
   item: OrderMenuInfo;
+  onChangeAmount?: (id: number, amount: number) => void;
 }
 
-export const ProductItem = ({ item }: Props) => {
+export const ProductItem = (props: Props) => {
   return (
     <li className="flex flex-row items-center px-4 py-3 text-base font-medium leading-none">
-      <span className="flex-1 w-0">{item.menuInfo.menuName}</span>
+      <span className="flex-1 w-0">{props.item.menuInfo.menuName}</span>
       <div className="flex flex-row items-center">
-        <Button color="primary-transparent" isNoPadding>
+        <Button
+          color="primary-transparent"
+          isNoPadding
+          onClick={() => props.onChangeAmount?.(props.item.menuInfo.menuId, -1)}
+        >
           <RemoveOutlined fontSize="small" />
         </Button>
-        <span className="w-7 text-center">{item.quantity}</span>
-        <Button color="primary-transparent" isNoPadding>
+        <span className="w-7 text-center">{props.item.quantity}</span>
+        <Button
+          color="primary-transparent"
+          isNoPadding
+          onClick={() => props.onChangeAmount?.(props.item.menuInfo.menuId, -1)}
+        >
           <AddOutlined fontSize="small" />
         </Button>
       </div>
-      <span className="w-20 text-right">{formatNumberWithComma(item.menuInfo.menuPrice * item.quantity)}</span>
+      <span className="w-20 text-right">
+        {formatNumberWithComma(
+          props.item.menuInfo.menuPrice * props.item.quantity,
+        )}
+      </span>
     </li>
   );
 };
--- src/components/payment/ReceiptPanel/components/ProductList/ProductList.tsx
+++ src/components/payment/ReceiptPanel/components/ProductList/ProductList.tsx
@@ -3,11 +3,16 @@ import { ProductItem } from '../ProductItem/ProductItem';
 
 export interface Props {
   items: OrderMenuInfo[];
+  onChangeAmount?: (id: number, amount: number) => void;
 }
 
 export const ProductList = (props: Props) => {
   const list = props.items.map((item, index) => (
-    <ProductItem key={index} item={item} />
+    <ProductItem
+      key={index}
+      item={item}
+      onChangeAmount={props.onChangeAmount}
+    />
   ));
   return <ul className="flex flex-col flex-1 h-0 overflow-y-auto">{list}</ul>;
 };
