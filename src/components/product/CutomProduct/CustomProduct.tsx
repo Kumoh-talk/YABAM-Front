dd41fc17ecc0843f92370d77a8d39fa8c392db60
wei756 <wei756fg@gmail.com>
2025-05-19 07:06:01 +0900

    feat: CustomProduct 클릭시 효과 추가

--- src/components/product/CutomProduct/CustomProduct.tsx
+++ src/components/product/CutomProduct/CustomProduct.tsx
@@ -11,7 +11,7 @@ export const CustomProduct = ({ item, onClick }: CustomProductProps) => {
   return (
     <div
       className={clsx(
-        'w-[16.5rem] p-4 gap-4 flex flex-col rounded-lg border border-neutral-300 select-none',
+        'w-[16.5rem] p-4 gap-4 flex flex-col rounded-lg border border-neutral-300 select-none active:border-primary',
         { 'opacity-50': item.menuIsSoldOut },
       )}
       onClick={() => !item.menuIsSoldOut && onClick?.(item.menuId)}
