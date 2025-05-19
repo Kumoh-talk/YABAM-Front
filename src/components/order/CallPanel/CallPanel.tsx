34ef1564459494f8743d537e4cc0d5fd205725f5
wei756 <wei756fg@gmail.com>
2025-05-20 00:12:40 +0900

    fix: 요청사항 효도모드

--- src/components/order/CallPanel/CallPanel.tsx
+++ src/components/order/CallPanel/CallPanel.tsx
@@ -6,7 +6,7 @@ export const CallPanel = () => {
   const { calls, handleCompleteCall } = useCallValues();
 
   return (
-    <section className="absolute left-4 bottom-0 w-[26rem] bg-white border border-gray-300 rounded-lg shadow-lg z-50">
+    <section className="absolute left-4 bottom-0 w-[36rem] bg-white border border-gray-300 rounded-lg shadow-lg z-50">
       <header className="flex flex-row justify-between items-center p-4 border-b border-gray-500 leading-none text-xl">
         <span className="font-medium">요청사항</span>
         {calls.length > 0 && (
@@ -20,7 +20,7 @@ export const CallPanel = () => {
           calls.map((call) => (
             <li
               key={call.callId}
-              className="flex flex-row justify-between items-center p-2 border-b border-gray-500 text-xl"
+              className="flex flex-row justify-between items-center p-2 border-b border-gray-500 text-3xl font-medium"
             >
               <span>
                 [{call.tableNumber}번 테이블] {call.callMessage}
