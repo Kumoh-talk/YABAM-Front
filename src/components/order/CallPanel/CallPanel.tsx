fdb45d5d36866043e5db7ff73b28909378f9b10a
wei756 <wei756fg@gmail.com>
2025-05-19 20:57:30 +0900

    fix: 요청사항 글자 크기 확대

--- src/components/order/CallPanel/CallPanel.tsx
+++ src/components/order/CallPanel/CallPanel.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@/components/common";
-import { useStoreValues } from "@/contexts/store/StoreContext";
-import { useCallValues } from "@/contexts/call/CallContext";
+import { Button } from '@/components/common';
+import { useCallValues } from '@/contexts/call/CallContext';
+import { CheckRounded } from '@mui/icons-material';
 
 export const CallPanel = () => {
   const { calls, handleCompleteCall } = useCallValues();
 
   return (
-    <section className="absolute left-4 bottom-0 w-[22rem] bg-white border border-gray-300 rounded-lg shadow-lg z-50">
-      <header className="flex flex-row justify-between items-center p-4 border-b border-gray-500 leading-none">
+    <section className="absolute left-4 bottom-0 w-[26rem] bg-white border border-gray-300 rounded-lg shadow-lg z-50">
+      <header className="flex flex-row justify-between items-center p-4 border-b border-gray-500 leading-none text-xl">
         <span className="font-medium">요청사항</span>
         {calls.length > 0 && (
           <div className="flex px-2 text-sm bg-red rounded-xl text-white">
@@ -20,18 +20,18 @@ export const CallPanel = () => {
           calls.map((call) => (
             <li
               key={call.callId}
-              className="flex flex-row justify-between items-center p-2 border-b"
+              className="flex flex-row justify-between items-center p-2 border-b border-gray-500 text-xl"
             >
               <span>
                 [{call.tableNumber}번 테이블] {call.callMessage}
               </span>
               <Button onClick={() => handleCompleteCall(call.callId)}>
-                확인
+                <CheckRounded />
               </Button>
             </li>
           ))
         ) : (
-          <li className="p-2 text-gray-400">요청사항이 없습니다</li>
+          <li className="px-4 py-2 text-text-secondary">요청사항이 없습니다</li>
         )}
       </ul>
     </section>
