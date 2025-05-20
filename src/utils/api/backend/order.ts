22b3da59f985971bec7969fe0997c20d5d7a2e13
wei756 <wei756fg@gmail.com>
2025-05-20 17:22:18 +0900

    style: singleQuote 적용

--- src/utils/api/backend/order.ts
+++ src/utils/api/backend/order.ts
@@ -3,93 +3,93 @@ import {
   OrderStatus,
   OrderInfo,
   OrderMenuStatus,
-} from "@/types/backend/order";
-import { api } from "./common";
+} from '@/types/backend/order';
+import { api } from './common';
 
 export const getOrders = async (
   saleId: number,
   perPage: number = 999,
-  statuses: OrderStatus[]
+  statuses: OrderStatus[],
 ) => {
   const res = await api<OrderSelectResponse>(
     `/yabam/api/v1/sales/${saleId}/orders?pageSize=${perPage}&orderStatuses=${statuses.join()}`,
-    "GET"
+    'GET',
   );
-  if ("success" in res && res.success == "true") {
+  if ('success' in res && res.success == 'true') {
     return res.data;
   }
-  throw new Error("주문 목록 조회 실패");
+  throw new Error('주문 목록 조회 실패');
 };
 
 export const createDirectOrder = async (
   receiptId: string,
-  menus: { menuId: number; menuQuantity: number }[]
+  menus: { menuId: number; menuQuantity: number }[],
 ) => {
   const res = await api<OrderInfo>(
     `/yabam/api/v1/receipts/${receiptId}/orders/direct`,
-    "POST",
-    menus
+    'POST',
+    menus,
   );
-  if ("success" in res && res.success === "true") {
+  if ('success' in res && res.success === 'true') {
     return res.data;
   }
-  throw new Error("직접 주문 추가 실패");
+  throw new Error('직접 주문 추가 실패');
 };
 
 export const updateOrderStatus = async (
   orderId: number,
-  status: OrderStatus
+  status: OrderStatus,
 ): Promise<OrderInfo> => {
   const res = await api<OrderInfo>(
     `/yabam/api/v1/orders/${orderId}/status?orderStatus=${status}`,
-    "PATCH",
+    'PATCH',
     {
       status: status,
-    }
+    },
   );
-  if ("success" in res && res.success === "true") {
+  if ('success' in res && res.success === 'true') {
     return res.data;
   }
-  throw new Error("주문 상태 업데이트 실패");
+  throw new Error('주문 상태 업데이트 실패');
 };
 
 export const updateOrderMenuStatus = async (
   orderMenuId: number,
-  status: OrderMenuStatus
+  status: OrderMenuStatus,
 ): Promise<OrderInfo> => {
   const res = await api<OrderInfo>(
     `/yabam/api/v1/order-menus/${orderMenuId}/status?orderMenuStatus=${status}`,
-    "PATCH"
+    'PATCH',
   );
-  if ("success" in res && res.success === "true") {
+  if ('success' in res && res.success === 'true') {
     return res.data;
   }
-  throw new Error("주문 메뉴 상태 업데이트 실패");
+  throw new Error('주문 메뉴 상태 업데이트 실패');
 };
 
 export const updateOrderMenuQuantity = async (
   orderMenuId: number,
-  patchQuantity: number
+  patchQuantity: number,
 ): Promise<OrderInfo> => {
   const res = await api<OrderInfo>(
     `/yabam/api/v1/order-menus/${orderMenuId}/quantity?patchQuantity=${patchQuantity}`,
-    "PATCH"
+    'PATCH',
   );
-  if ("success" in res && res.success === "true") {
+  if ('success' in res && res.success === 'true') {
     return res.data;
   }
-  throw new Error("주문 메뉴 수량 변경 실패");
+  throw new Error('주문 메뉴 수량 변경 실패');
 };
 
 export const deleteOrderMenu = async (
-  orderMenuId: number
+  orderMenuId: number,
 ): Promise<OrderInfo> => {
   const res = await api<OrderInfo>(
     `/yabam/api/v1/order-menus/${orderMenuId}`,
-    "DELETE"
+    'DELETE',
   );
-  if ("success" in res && res.success === "true") {
+  if ('success' in res && res.success === 'true') {
     return res.data;
   }
-  throw new Error("주문 메뉴 삭제 실패");
+  throw new Error('주문 메뉴 삭제 실패');
 };
