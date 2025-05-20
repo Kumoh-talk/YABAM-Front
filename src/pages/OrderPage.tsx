9df0ca385d4a367f08d8a92cd8f84ba43387d17d
Y-seungyi <kimche9865@naver.com>
2025-05-20 18:33:01 +0900

    chore: TableView order제거

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -42,7 +42,6 @@ export const OrderPage = () => {
           />
         ) : (
           <TableView
-            orders={orders}
             onChangeSelectedTable={setSelectedTableId}
           />
         )}
